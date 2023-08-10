8e315a423a25b6e8986fc47d84d54152cf92facb
root <wessamabdelnabi903@gmail.com>
2023-08-10 16:51:39 -0700

    adv2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (not Success)
  */
 int main(void)
 {
-fputs("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+fputs("and that piece of art is useful"\- Dora Korpar, 2015-10-19\n");
 return (1);
 }
