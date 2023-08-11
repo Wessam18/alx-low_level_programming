cd8b7ceec330c8616b5a101fd2ec3f2b3ccdd379
root <wessamabdelnabi903@gmail.com>
2023-08-10 17:07:28 -0700

    adv2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - A c program that prints a line to the standard error
@@ -7,6 +8,6 @@
  */
 int main(void)
 {
-fputs("and that piece of art is useful\"- Dora Korpar, 2015-10-19\n");
+fputs("and that piece of art is useful\"- Dora Korpar, 2015-10-19\n , stdout");
 return (1);
 }
