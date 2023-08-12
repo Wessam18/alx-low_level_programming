b94250d525bdebeb9d2d528ff8ee0e434b8c255f
root <wessamabdelnabi903@gmail.com>
2023-08-11 18:12:52 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*
+* main - entry point
+*
+* Description - print  print digit number of base 10
+*
+*Return: Always 0 (success)
+*
+*/
+int main(void)
+{
+int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+for (n = 0 ; n < 10 ; n++)
+	printf("\n%d", n);
+	return (0);
+}
