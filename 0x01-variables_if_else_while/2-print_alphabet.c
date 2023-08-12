5a8b9202866e563fbf230603f6307e69968e5396
root <wessamabdelnabi903@gmail.com>
2023-08-11 19:15:14 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/*
+* main - prints the alphabet in lowercase,
+*followed by a new line
+*
+*Return: Always 0 (Success)
+*
+*/
+int main(void)
+{
+int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
