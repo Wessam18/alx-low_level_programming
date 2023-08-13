4923fc07bc07a2b6f522e64e33d1fd522b0e7731
root <wessamabdelnabi903@gmail.com>
2023-08-13 14:47:46 -0700

    7

--- 0x01-variables_if_else_while/7-print_tebaphla.c
+++ 0x01-variables_if_else_while/7-print_tebaphla.c
@@ -10,11 +10,9 @@
 */
 int main(void)
 {
-char ch = 'a';
-for (ch = 'a' ; ch <= 'z' ; ch++)
-	putchar(ch);
-for (ch = 'A' ; ch <= 'Z' ; ch++)
-	putchar(ch);
+char ch = 'z';
+for (ch = 'z' ; ch >= 'z' ; ch++)
+        putchar(ch);
 putchar('\n');
 return (0);
 }
