8e7b2478b944f2353909f0d3adcbeb4fec33a456
root <wessamabdelnabi903@gmail.com>
2023-08-13 14:52:12 -0700

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+* main - entry point
+*
+* Description - A c program print alphabet in lowercase
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char ch = 'z';
+for (ch = 'z' ; ch >= 'z' ; ch++)
+	putchar(ch);
+putchar('\n');
+return (0);
+}
