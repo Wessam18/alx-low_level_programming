cf857fdbb398274c2a3b39a9d56110a2868e77bb
root <wessamabdelnabi903@gmail.com>
2023-08-15 08:23:34 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include  <stdio.h>
+#define A (_putchar)
+/**
+*Description - A c program print _putchar
+*main - entery point
+*
+*return = always 0 (success)
+*
+*/
+
+int main(void)
+{
+char ch = 'A';
+	putchar (ch, \n);
+return (0);
+}
