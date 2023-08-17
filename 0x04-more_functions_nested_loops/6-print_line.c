ddb0c7f115a949ba11c798f161ed10806e8ba9cf
root <wessamabdelnabi903@gmail.com>
2023-08-17 11:37:26 -0700

    6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -11,13 +11,14 @@ void print_line(int n)
 int count;
 
 if (n <= 0)
-{
 	_putchar('\n');
-}
+
 else
-	for (count = 1; count <= n; count++)
 {
-	_putchar('_');
+	for (count = 1; count <= n; count++)
+	{
+		_putchar('_');
+	}
 	_putchar('\n');
 }
 }
