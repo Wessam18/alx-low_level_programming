91e07fb75a9dc7bb7dcaa53bf817d3df9033f0c1
root <wessamabdelnabi903@gmail.com>
2023-08-17 08:46:14 -0700

    5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,15 +7,17 @@ void more_numbers(void)
 int i = 0;
 while (i < 10)
 {
-	int c;
+	int num,count;
 
-	for (c = 0; c <= 14; c++)
+	for (num = 0; num <= 14; c++)
 	{
-		if (c > 9)
+		num = count;
+		if (count > 9)
 		{
 		_putchar(1 + '48');
+		num = count + %10;
 		}
-		_putchar(c + 48);
+		_putchar(num + 48);
 	}
 	i++;
 	_putchar('\n');
