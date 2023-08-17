29131a5947203f38f4ea433a2eb5a7848b9f2982
root <wessamabdelnabi903@gmail.com>
2023-08-17 14:37:30 -0700

    9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+*main - program that print from 0 to 100
+*
+*Return: always 0 (success)
+*
+*/
+int main(void)
+{
+int n;
+
+	for (n = 0; n <= 0; n++)
+	{
+		if ((n % 3) == 0)
+			printf("Fizz");
+		else if ((n % 5) == 0)
+			printf("Buzz");
+		else if (((n % 3) == 0 && (n % 5) == 0))
+			printf("FizzBuzz");
+		else
+			printf("%d", n);
+
+	if (n != 100)
+		printf(" ");
+	else
+		printf("/n");
+	}
+	return (0);
+}
