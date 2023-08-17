40a8535b04ddc6313e779b026f8894dc383ba6e0
root <wessamabdelnabi903@gmail.com>
2023-08-17 09:10:43 -0700

    5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,21 +1,23 @@
 #include "main.h"
 /**
 *more_numbers - Prints the numbers ten times followed by a newl
+*
+*
+*Return: always 0 (success)
 */
 void more_numbers(void)
 {
 int i = 0;
 while (i < 10)
 {
-	int num,count;
+	int num;
 
-	for (num = 0; num <= 14; c++)
+	for (num = 0; num <= 14; num++)
 	{
-		num = count;
-		if (count > 9)
+		if (num > 9)
 		{
-		_putchar(1 + '48');
-		num = count + %10;
+		_putchar(1 + 48);
+		num = num % 10;
 		}
 		_putchar(num + 48);
 	}
