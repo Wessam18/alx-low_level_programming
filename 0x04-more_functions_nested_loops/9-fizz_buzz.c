bf92547870f80c90f9b1c349cf920ea0696b38b8
root <wessamabdelnabi903@gmail.com>
2023-08-17 15:17:32 -0700

    9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,21 +9,29 @@ int main(void)
 {
 int n;
 
-	for (n = 0; n <= 0; n++)
+	for (n = 0; n <= 100; n++)
 	{
-		if ((n % 3) == 0)
+		if ((n % 3 == 0) && (n % 5 != 0))
+		{
 			printf("Fizz");
-		else if ((n % 5) == 0)
+			printf(" ");
+		}
+		else if ((n % 5 == 0) && (n % 3 != 0))
+		{
 			printf("Buzz");
-		else if (((n % 3) == 0 && (n % 5) == 0))
+			printf(" ");
+		}
+		else if ((n % 3 == 0) && (n % 5 == 0))
+		{
 			printf("FizzBuzz");
+			printf(" ");
+		}
 		else
+		{
 			printf("%d", n);
-
-	if (n != 100)
-		printf(" ");
-	else
-		printf("/n");
+			printf(" ");
+		}
 	}
+	printf("/n");
 	return (0);
 }
