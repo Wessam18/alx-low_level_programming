c3a211cf8234bdc6f31a8e5fc40276c075a0c0bf
root <wessamabdelnabi903@gmail.com>
2023-08-21 17:40:41 -0700

    3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_puts -  function that prints a string, followed by a new line
+ *
+ *@str: reper to string
+ *
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str + 0);
+		str++;
+	}
+	_putchar('\n');
+}
