5f7b2fc0eeb426ef77628dcc36950bffde287e5a
root <wessamabdelnabi903@gmail.com>
2023-08-24 18:40:08 -0700

    3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *_strcmp - function compare 2 string
+ *
+ *@s1: string one
+ *@s2: string two
+ *
+ *Return: return 0 if equal
+ */
+int _strcmp(char *s1, char *s2)
+{
+int i = 0;
+
+	while (s1[i] == s2[i])
+	{
+		s1++;
+		s2++;
+		return (0);
+	}
+	return (s1[i] - s2[i]);
+}
