446b1640e1f80da920e3067217fe6bf5b99acb4b
root <wessamabdelnabi903@gmail.com>
2023-08-24 19:27:30 -0700

    3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,10 +11,13 @@ int _strcmp(char *s1, char *s2)
 {
 int i = 0;
 
-	while (s1[i] == s2[i])
+	while (*s1)
 	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
 		i++;
-		return (s1[i] - s2[i]);
 	}
 	return (0);
 }
