5b7e998e0dd5d8a2f22d3e02b125eeeea1533bc9
root <wessamabdelnabi903@gmail.com>
2023-08-28 16:57:05 -0700

    5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -17,9 +17,10 @@ int j = 0;
 		for (j = 0; needle[j] != '\0'; j++)
 		{
 			if (haystack[i + j] != needle[j])
-
-				return (needle + j);
+				break;
 		}
+
+		return (needle + j);
 	}
 	return (NULL);
 }
