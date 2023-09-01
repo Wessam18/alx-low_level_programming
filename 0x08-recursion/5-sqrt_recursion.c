74f5a133c98ad709a7df55ee2b57ad9809d74805
wessam18 <wessamabdelnabi903@gmail.com>
2023-09-01 04:51:07 +0300

    "5"

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,32 +1,48 @@
 #include "main.h"
+
 /**
- * _sqrt_recursion - get square root of a number.
+ * squareroot - a function that returns the natural square root of a number.
+ *
  * @n: number.
- * Return: (The result)
-*/
+ * @m: int used to find sqrt.
+ * Return: returns the natural square root of a number
+ * If n does not have a natural square root,
+ * the function should return -1
+ *
+ */
+int squareroot(int n, int m)
+{
+if (m * m == n)
+{
+return (m);
+}
+if (m * m < n)
+{
+return (squareroot(n, m + 1));
+}
+
+return (-1);
+
+}
+
+/**
+ * _sqrt_recursion - a function that returns
+ * the natural square root of a number.
+ *
+ * @n: number.
+ *
+ * Return: returns the natural square root of a number
+ * If n does not have a natural square root,
+ * the function should return -1
+ *
+ */
+
 int _sqrt_recursion(int n)
 {
-return (_sqrt_recursion(n, 0));
-}
-/**
- * ft_sqrt_recursion - function get square root.
- * @n: input value to get square root of it.
- * @b: intermediate value.
- * Return: (value).
-*/
-
-int ft_sqrt_recursion(int n, int b)
-{
-if (b * b == n)
-{
-return (b);
-}
-else if (b * b > n)
+if (n < 0)
 {
 return (-1);
 }
-else
-{
-return (ft_sqrt_recursion(n, b + 1));
-}
+
+return (squareroot(n, 0));
 }
