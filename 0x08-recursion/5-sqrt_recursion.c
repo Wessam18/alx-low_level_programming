e45a0a425bbd92c7ae00d0ab46189fbb70dd4cf7
wessam18 <wessamabdelnabi903@gmail.com>
2023-09-01 04:42:36 +0300

    "5"

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - get square root of a number.
+ * @n: number.
+ * Return: (The result)
+*/
+int _sqrt_recursion(int n)
+{
+return (ft_sqrt_recursion(n, 0));
+}
+/**
+ * ft_sqrt_recursion - function get square root.
+ * @n: input value to get square root of it.
+ * @b: intermediate value.
+ * Return: (value).
+*/
+
+int ft_sqrt_recursion(int n, int b)
+{
+if (b * b == n)
+{
+return (b);
+}
+else if (b * b > n)
+{
+return (-1);
+}
+else
+{
+return (ft_sqrt_recursion(n, b + 1));
+}
+}
