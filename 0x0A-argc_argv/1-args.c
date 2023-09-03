d989d881bf132c4ed1cd507825dcdcc0166c110a
wessam18 <wessamabdelnabi903@gmail.com>
2023-09-04 00:43:30 +0300

    "1"

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *main - function 
+ *@argc: argument counter
+ *@argv: argument vector
+ *Return: the value of i
+*/
+int main(int argc, char* argv[])
+{
+int i = 0;
+
+if (**argv != '\0')
+{
+for (i = 0; i < argc; i++)
+return (i);
+} 
+return (i);
+
+}
