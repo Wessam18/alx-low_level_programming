f5a0b573bbd16eb1b70c429f3b5ebc665e777f9d
wessam18 <wessamabdelnabi903@gmail.com>
2023-09-06 02:55:56 +0300

    "3"

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
  *alloc_grid - function get 2-dimentional array
- * 
+ *
  *@height: height of array
  *@width: width of array
  *Return: null
- * 
+ *
 */
 int **alloc_grid(int width, int height)
 {
@@ -14,20 +14,19 @@ int **m, i = 0, j = 0;
 if (width <= 0 || height <= 0)
 return (NULL);
 
-m = (int **) malloc(height *sizeof(int));
+m = (int **) malloc(height * sizeof(int*));
 if (m == NULL)
 return (NULL);
 
 for (i = 0; i < height; i++)
 {
-m[i] = (int *) malloc(width *sizeof(int));
+m[i] = (int *) malloc(width * sizeof(int));
 if (m[i] == NULL)
 return (NULL);
 }
 for (i = 0; i < height; i++)
 for (j = 0; j < width; j++)
-{
+;
 m[i][j] = '\0';
-}
 return (m);
 }
