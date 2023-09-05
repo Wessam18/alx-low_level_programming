7728dd6282cba505bc8de1d4846bc28c301c1f81
wessam18 <wessamabdelnabi903@gmail.com>
2023-09-05 04:10:51 +0300

    "1"

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
- *main - function 
  *@argc: argument counter
  *@argv: argument vector
  *Return: the value of i
 */
-int main(int argc, void char* argv[])
+int main(int argc, char *argv[])
 {
+void char *argv;
+
 printf("%d\n", argc -1);
 return (0);
 }
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ *_strdup - function duplicate string
+ * 
+ *@str: string
+ *Return: null
+ * 
+*/
+char *_strdup(char *str)
+{
+int x, i;
+char *m;
+if (str == NULL)
+{
+return (NULL);
+}
+
+for (x = 0; str[x] != '\0'; x++)
+{
+m = malloc(x * sizeof *(str + 1));
+
+if (m == 0)
+{
+return (NULL);
+}
+else 
+{
+for (i = 0; i < x; i++)
+m[i] = str[i];
+}
+return (m);
+}
+return (0);
+}
