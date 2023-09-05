53b8dd34719f3df7dba27f55b0b97bc6d5b6196f
wessam18 <wessamabdelnabi903@gmail.com>
2023-09-05 18:02:15 +0300

    "0"

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,7 @@ char *create_array(unsigned int size, char c)
 {
 char *x;
 x = (char *) malloc(size * sizeof(char));
-if (size == 0 || x == 0)
+if (size == 0)
 return (NULL);
 while (size--)
 x[size] = c;
