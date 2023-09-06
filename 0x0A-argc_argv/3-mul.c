afa11c6fcc570bb619d7eca4846fc7170c9ddcbc
wessam18 <wessamabdelnabi903@gmail.com>
2023-09-07 02:35:20 +0300

    "3"

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - function that multiplay 2 numbers
+ *
+ *@argc: argument count
+ *@argv: argument vector
+ *
+ *Return: always 0
+*/
+int main(int argc, char *argv[])
+{
+
+if (argc < 3)
+{
+    printf("Error\n");
+    return (1);
+}
+else
+{
+    printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
+}
+return (0);
+}
