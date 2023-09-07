dba33a21331abc6507a0ab1cf2e0dfb8471661be
wessam18 <wessamabdelnabi903@gmail.com>
2023-09-07 21:50:50 +0300

    "1"

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *_strlen - get length
+ *@s: string
+ *Return: length
+*/
+int _strlen(char *s)
+{
+int len;
+for (len = 0; s[len] != '\0'; len++)
+;
+return (len);
+}
+
+/**
+ *string_nconcat - function concatinate strings
+ *
+ *@s1: string 1
+ *@s2: string 2
+ *@n: length of s2 printed
+ *Return: null
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+unsigned int i;
+unsigned int len1;
+char *m;
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+len1 = _strlen(s1);
+m = malloc((len1 + n) *sizeof(char) + 1);
+if (m == 0)
+{
+return (NULL);
+}
+for (i = 0; i <= (len1 + n); i++)
+{
+if (i < len1)
+m[i] = s1[i];
+else
+m[i] = s2[i - len1];
+}
+m[i] = '\0';
+return (m);
+}
