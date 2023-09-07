98885816bbf4222c624c25cc63b208c435214fbe
wessam18 <wessamabdelnabi903@gmail.com>
2023-09-07 21:02:11 +0300

    "0"

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *malloc_checked - program checked memory
+ *
+ *@b: integer allocated
+ *Return: null
+ *
+*/
+void *malloc_checked(unsigned int b)
+{
+unsigned int *x;
+
+x = malloc(b);
+if (x == 0)
+{
+exit(98);
+}
+else
+return (x);
+}
