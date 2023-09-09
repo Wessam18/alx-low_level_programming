fb94cf4b153d959a88add5521f8b997d2f69481d
wessam18 <wessamabdelnabi903@gmail.com>
2023-09-09 09:10:05 +0300

    "100"

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,40 +8,42 @@
 void print_times_table(int n)
 {
 int m, i;
-    if (n == 15 || n < 0)
-    {
-        return;
-    }
+	if (n == 15 || n < 0)
+	{
+		return;
+	}
 	for (i = 0; i <= n; i++)
 	{
 		for (m = 0; m <= n; m++)
 		{
 			if (m == 0)
-			_putchar('0');
-			else if ((i * m) <= 9)
 			{
+			_putchar('0');
+			}
+		else if ((i * m) <= 9)
+		{
 			_putchar(',');
 			_putchar(' ');
-            _putchar(' ');
+			_putchar(' ');
 			_putchar(' ');
 			_putchar((i * m) + '0');
-			}
-			else if ((i * m) >= 9 && (i * m) <= 99)
-			{
+		}
+		else if ((i * m) >= 9 && (i * m) <= 99)
+		{
 			_putchar(',');
 			_putchar(' ');
-            _putchar(' ');
+			_putchar(' ');
 			_putchar(((n * m) / 100) + '0');
-            _putchar(((n * m) / 10) + '0');
+			_putchar(((n * m) / 10) + '0');
 			_putchar(((n * m) % 10) + '0');
-			}
-            else 
-            {
-             _putchar(',');
+		}
+		else
+		{
+			_putchar(',');
 			_putchar(' ');
 			_putchar(((n * m) / 10) + '0');
-			_putchar(((n * m) % 10) + '0');   
-            }
+			_putchar(((n * m) % 10) + '0');
+		}
 		}
 		_putchar('\n');
 	}
