9f45d5f5f3020f6b9241fd809a83c16264bac654
wessam18 <wessamabdelnabi903@gmail.com>
2023-09-14 04:03:10 +0300

    "2"

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -6,15 +6,19 @@
  *@size: the size of the array
  *@cmp: pointer to the function you need to use
  *
+ *
 */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-long unsigned int x;
+int x;
+
 if (array != NULL && cmp != NULL)
 {
 for (x = 0; x < size; x++)
 {
-cmp(array[x]);
+if (cmp(array[x]) != 0)
+return (x);
 }
 }
+return (-1);
 }
