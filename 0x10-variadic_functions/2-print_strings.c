437104eacf6fed8261755bd61c9373552b28d1b4
wessam18 <wessamabdelnabi903@gmail.com>
2023-09-15 05:26:18 +0300

    "2"

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+/**
+ *print_strings - function sum all no.
+ *
+ *@n: numbers of args
+ *@separator: the string
+ *
+*/
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+va_list ap;
+unsigned int i;
+char *s;
+
+va_start(ap, n);
+
+for (i = 0; i < n; i++)
+{
+s = va_arg(ap, char *);
+
+if (s == NULL)
+printf("(nil)");
+
+printf("%s", s);
+
+if (i != (n - 1) && separator != NULL)
+printf("%s", separator);
+}
+printf("\n");
+va_end(ap);
+}
