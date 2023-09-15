9504153f471ed5deb47f211fe623abc943ae28d5
wessam18 <wessamabdelnabi903@gmail.com>
2023-09-15 05:02:08 +0300

    "1"

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,12 +15,10 @@ va_start(ap, n);
 
 for (i = 0; i < n; i++)
 {
-if (i != (n - 1) && separator == NULL)
-{
-printf("%d%s", va_arg(ap, int), separator);
-}
-else
 printf("%d", va_arg(ap, int));
+
+if (i != (n - 1) && separator != NULL)
+printf("%s", separator);
 }
 printf("\n");
 va_end(ap);
