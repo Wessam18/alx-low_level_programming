80e71de6916e7ae5a8177ee6c144a4722cd4abf2
wessam18 <wessamabdelnabi903@gmail.com>
2023-09-22 03:39:15 +0300

    "0"

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ * 
+ * 
+ * 
+ * 
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+list_t *first_node;
+first_node = malloc(sizeof(list_t));
+
+    if (head == NULL)
+    {
+        return (NULL);
+    }
+    if (first_node == NULL)
+    {
+        return (head);
+    }
+    if (str != NULL)
+    {
+        first_node -> str = strdup(str);
+        if (first_node -> str != NULL)
+        
+    }
+
+}
