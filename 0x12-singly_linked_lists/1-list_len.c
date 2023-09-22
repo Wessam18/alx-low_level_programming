0ab18da93f7426be177b376048a06c562f009be9
wessam18 <wessamabdelnabi903@gmail.com>
2023-09-22 04:06:33 +0300

    "1"

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -10,13 +10,15 @@ size_t list_len(const list_t *h)
 size_t i = 0;
 
 if (h == NULL)
-	{
-		return (0);
-	}
-while (h -> next != NULL)
-	{
-		h = h -> next;
-		i++;
-	}
+{
+	return (0);
+}
+
+while (h->next != NULL)
+{
+	h = h->next;
+	i++;
+}
+
 	return (i);
 }
