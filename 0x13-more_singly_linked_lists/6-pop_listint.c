58cea47ffc882978ad156a32c241977a9b5ab485
wessam18 <wessamabdelnabi903@gmail.com>
2023-09-26 18:43:48 +0300

    "6"

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -2,22 +2,24 @@
 /**
  *pop_listint - fun frees the list
  *@head: start of the list
+ *Return: data
 */
-int pop_listint(listint_t **head);
+int pop_listint(listint_t **head)
 {
 listint_t *temp;
+int data;
 
-    if (*head == NULL)
-    {
-        return (0);
-    }
+	if (*head == NULL)
+	{
+		return (0);
+	}
 
-	while (head != NULL)
+	while ((*head) != NULL)
 	{
 		temp = (*head)->next;
-        data = (*head)->n;
+		data = (*head)->n;
 		free(*head);
-
+		*head = temp;
 	}
-    return (data);
+	return (data);
 }
