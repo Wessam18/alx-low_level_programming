eb47c72bd4b34d4e006bd3c0cc6c3155a467a07d
wessam18 <wessamabdelnabi903@gmail.com>
2023-09-26 20:49:58 +0300

    "7"

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ *get_nodeint_at_index - fun frees the list
+ *@head: start of the list
+ * @index: num of nodes
+ *Return: nodes
+*/
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+unsigned int i = 0;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < index; i++)
+	{
+		head = head->next;
+	}
+
+	return (head);
+}
