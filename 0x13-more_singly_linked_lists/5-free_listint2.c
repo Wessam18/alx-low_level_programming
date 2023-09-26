feed94cd97b8d04183f8e1c583249cc1ff100aff
wessam18 <wessamabdelnabi903@gmail.com>
2023-09-26 17:43:05 +0300

    "9"

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,17 +1,20 @@
 #include "lists.h"
 /**
- *free_listint - fun frees the list
+ *free_listint2 - fun frees the list
  *@head: start of the list
 */
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
+	
+	if (head == NULL)
+		return;
 
 	while (head != NULL)
 	{
 		temp = *head;
-		*head = temp->next;
+		*head = (*head)->next;
 		free(temp);
 	}
-    head = NULL;
+	*head = NULL;
 }
