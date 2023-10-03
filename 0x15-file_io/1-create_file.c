eca1a69a44f512061d4a507cb872e17ae0632879
wessam18 <wessamabdelnabi903@gmail.com>
2023-10-03 04:53:32 +0300

    "1"

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * create_file - function Create a file
+ * @filename: file name to create.
+ * @text_content: text to be written
+ * Return: as need
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		bytes = write(fd, text_content, strlen(text_content));
+		if (bytes == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+}
