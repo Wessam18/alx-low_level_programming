493dbb75f900e70ef7f8a0e195ec4f8bdcd1eb06
wessam18 <wessamabdelnabi903@gmail.com>
2023-10-03 04:56:00 +0300

    "1"

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,6 @@ int create_file(const char *filename, char *text_content)
 		bytes = write(fd, text_content, strlen(text_content));
 		if (bytes == -1)
 		{
-			close(fd);
 			return (-1);
 		}
 	}
