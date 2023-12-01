a3b074724040ac5bbd4264576a4b2a9c10c6785e
wessam18 <wessamabdelnabi903@gmail.com>
2023-12-01 23:36:34 +0200

    "3"

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,53 +9,42 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    unsigned long int index;
-    hash_node_t *new_node, *temp;
+unsigned long int index;
+hash_node_t *new_node, *temp;
 
-    if (ht == NULL || key == NULL || *key == '\0')
-        return 0;
-
-    index = key_index((const unsigned char *)key, ht->size);
-    
-    /* Check if key already exists in the hash table */
-    temp = ht->array[index];
-    while (temp != NULL)
-    {
-        if (strcmp(temp->key, key) == 0)
-        {
-            /* Key already exists, update the value */
-            free(temp->value);  /* Free existing value */
-            temp->value = strdup(value);  /* Duplicate and set new value */
-            if (temp->value == NULL)
-                return 0; /* Memory allocation for value failed */
-            return 1; /* Update successful */
-        }
-        temp = temp->next;
-    }
-
-    /* Key does not exist, create a new node */
-    new_node = malloc(sizeof(hash_node_t));
-    if (new_node == NULL)
-        return 0; /* Memory allocation for new node failed */
-
-    new_node->key = strdup(key);
-    if (new_node->key == NULL)
-    {
-        free(new_node);
-        return 0; /* Memory allocation for key failed */
-    }
-
-    new_node->value = strdup(value);
-    if (new_node->value == NULL)
-    {
-        free(new_node->key);
-        free(new_node);
-        return 0; /* Memory allocation for value failed */
-    }
-
-    /* Add the new node at the beginning of the linked list */
-    new_node->next = ht->array[index];
-    ht->array[index] = new_node;
-
-    return 1; /* Addition successful */
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	temp = ht->array[index];
+	while (temp != NULL)
+	{
+		if (strcmp(temp->key, key) == 0)
+		{
+			free(temp->value);  /* Free existing value */
+			temp->value = strdup(value);  /* Duplicate and set new value */
+			if (temp->value == NULL)
+				return (0); /* Memory allocation for value failed */
+			return (1); /* Update successful */
+		}
+		temp = temp->next;
+	}
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0); /* Memory allocation for new node failed */
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (0); /* Memory allocation for key failed */
+	}
+	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (0); /* Memory allocation for value failed */
+	}
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
 }
