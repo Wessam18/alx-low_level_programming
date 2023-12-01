604f6f5d496e299679b256f1c6e7de94e2df1d21
wessam18 <wessamabdelnabi903@gmail.com>
2023-12-02 00:03:02 +0200

    "3"

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,7 +12,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 unsigned long int index;
 hash_node_t *new_node, *temp;
 
-	if (ht == NULL || key == NULL || value == '\0')
+	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
 	index = hash_djb2((const unsigned char *)key) % ht->size;
 	temp = ht->array[index];
