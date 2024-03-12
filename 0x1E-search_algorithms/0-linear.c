b719d007d5f0191badacbdfd9db644a04bf6bd31
wessam18 <wessamabdelnabi903@gmail.com>
2024-03-12 21:32:42 +0200

    task 0 - linear search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,19 @@
+#include "search_algos.h"
+/**
+ * 
+*/
+int linear_search(int *array, size_t size, int value)
+{
+size_t i = 0;
+
+    if (!array)
+        return (-1);
+
+    for (i = 0; i <= size; i++)
+    {
+        fprintf(stdout, "Value checked array[%lu] = [%d]\n", i, array[i]);
+        if (array[i] == value)
+            return (i);
+    }
+    return (-1);
+}
