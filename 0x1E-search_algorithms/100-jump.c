ac889e2a80738aac1d6e3f1d03b04e9240740551
wessam18 <wessamabdelnabi903@gmail.com>
2024-03-13 21:37:09 +0200

    edit jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -9,29 +9,28 @@
  */
 int jump_search(int *array, size_t size, int value)
 {
-    size_t jump = sqrt(size);
-    size_t left = 0;
-    size_t right = 0, i;
+size_t jump = sqrt(size);
+size_t left = 0;
+size_t right = jump, i;
 
-    if (!array || size == 0)
-        return (-1);
+	if (!array || !size || !value)
+		return (-1);
 
-    while (right < size && array[right] < value)
-    {
-        printf("Value checked array[%lu] = [%d]\n", right, array[right]);
-        left = right; /* Move the left boundary of the block */
-        right += jump; /* Move the right boundary by the jump step */
-    }
+	while (right < size && array[right] < value)
+	{
+		printf("Value checked array[%lu] = [%d]\n", right, array[right]);
+		left = right;
+		right += jump;
+	}
+	printf("Value found between indexes [%lu] and [%lu]\n", left, right);
 
-    printf("Value found between indexes [%lu] and [%lu]\n", left, right);
 
-    /* Linear search within the block */
-    for (i = left; i <= right && i < size; i++)
-    {
-        printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-        if (array[i] == value)
-            return (i); /* Found the value */
-    }
-
-    return (-1); /* Value not found */
+	/* Linear search within the block */
+	for (i = left; i <= right && i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i); /* Found the value */
+	}
+	return (-1); /* Value not found */
 }
