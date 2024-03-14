320bb8d903033c0649a7a8640f0ec405388297cc
wessam18 <wessamabdelnabi903@gmail.com>
2024-03-14 02:15:58 +0200

    .

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -10,7 +10,7 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
-int binary_recursion(int *array, size_t low, size_t high, int value);
+int binary_recur(int *array, size_t left, size_t right, int value);
 
 
 
